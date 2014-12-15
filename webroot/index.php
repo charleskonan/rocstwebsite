d5a83a6dd9061dcc968a3e4ef4a328990f641c75
i@mchris <christiankouacou@gmail.com>
2014-12-15 20:13:19 +0000

    changer lien
    changement du lien

--- webroot/index.php
+++ webroot/index.php
@@ -10,7 +10,7 @@ define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
 define('TIME_ZONE', 'Africa/Abidjan');
 
 //NEW CONSTANTES 
-define('rocstinteractive', 'http://'.$_SERVER['HTTP_HOST'].'/rocst/');
+define('rocstinteractive', 'http://'.$_SERVER['HTTP_HOST'].'/rocstwebsite/');
 
 /*define('CDNLINK', 'http://'.$_SERVER['HTTP_HOST'].'/CDN_BOM/');
 define('CDN','C:'.DS.'xampp'.DS.'htdocs'.DS.'CDN_BOM'.DS);*/
